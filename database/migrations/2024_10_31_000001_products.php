68520ea51b0ba23f00c77996e728973c0b34d7d7
Otavioglara <143605097+Otavioglara@users.noreply.github.com>
2024-11-02 22:47:16 -0300

    formatando o codigo

--- database/migrations/2024_10_31_000001_products.php
+++ database/migrations/2024_10_31_000001_products.php
@@ -6,27 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
-        //
+
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
             $table->string('description')->nullable();
-            $table->double('price')->nullable(); 
+            $table->double('price')->nullable();
             $table->unsignedBigInteger('category_id')->nullable();
+            $table->string('image');
             $table->timestamps();
         });
-    
-    
+
+
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('description')->nullable(); 
-            $table->enum('is_available', ['true', 'false']); 
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
